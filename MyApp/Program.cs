31e99ddd178402eacfc6ad49b74323c3bc790340
Prathvi Puthran <prathvi.puthran@planview.com>
2024-08-28 12:15:57 -0400

    First Project Added

---
+++ MyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using MyApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        int result = calculator.Add(5, 3);
+        Console.WriteLine($"The result of adding 5 and 3 is: {result}");
+    }
+}
